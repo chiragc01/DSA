6d7e773634f292e687ed437abb5b38373db9f99f
Chirag <chiragchaudhary2607@gmail.com>
2024-09-26 12:46:37 +0530

    pointers

---
+++ DSA/dsa/pointers/pointer1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+    int main(){
+        float price=100.99;
+        float* ptr=&price;
+        cout<<ptr<<endl;
+        cout<<&price;
+         return 0;
+    }
+   
